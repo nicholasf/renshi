cb6bdb3ead47419d6e784e3861025544f5ed82cc
Nicholas Faiz <nicholas.faiz@gmail.com>
2009-05-19 21:19:43 +1000

    better readme

--- lib/renshi.rb
+++ lib/renshi.rb
@@ -1,8 +1,7 @@
 $:.unshift(File.dirname(__FILE__)) unless
   $:.include?(File.dirname(__FILE__)) || $:.include?(File.expand_path(File.dirname(__FILE__)))
 
-# Renshi's aim is to be a lightweight library with dependencies solely on 
-# nokogiri.  
+# Renshi's aim is to be a lightweight library with dependencies solely on nokogiri.  
 require 'nokogiri'
 require 'renshi/parser'
 require 'renshi/node'
