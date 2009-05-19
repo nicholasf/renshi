8c82749025e14ceefedb7c1cbe31c5254ae9e56c
Nicholas Faiz <nicholas.faiz@gmail.com>
2009-05-19 14:32:51 +1000

    vaguely working with

--- lib/renshi.rb
+++ lib/renshi.rb
@@ -1,5 +1,12 @@
 $:.unshift(File.dirname(__FILE__)) unless
   $:.include?(File.dirname(__FILE__)) || $:.include?(File.expand_path(File.dirname(__FILE__)))
+
+# Renshi's aim is to be a lightweight library with dependencies solely on 
+# nokogiri.
+#
+# The Renshi parser tries to respect speed without complexity. It takes the 
+# approach that transformations should be done as they are encountered (much 
+# like a SAXParser) rather than preparing a tree of transformations.
   
 require 'nokogiri'
 require 'renshi/conditional_expressions'
@@ -21,7 +28,6 @@ module Renshi
   end
   
   def self.transform_node(node, context)
-    
     if node.attributes
       expressions = node.commands
       for expression in expressions 
@@ -30,26 +36,7 @@ module Renshi
     end
     
     if node.text?
-      refs = node.text.split("$")
-
-      if refs.size > 1
-        refs.each do |ref|
-          next if ref.empty? or ref.strip.empty?
-          
-          # if ref[0] == "{"
-          #   
-          # end
-          
-          words = ref.split(/(\s+)/)
-          val = eval words.first, context
-          words[0] = val
-          idx = refs.index(ref)
-          refs[idx] = words.join
-          ref = words.join
-        end
-      end
-      
-      node.content = refs.join
+      node.interpret(context)
     end
     
     node.children.each {|child| transform_node(child, context)}
@@ -66,7 +53,22 @@ module Renshi
     end
   end
   
-  class RenshiError < StandardError; end  
+  class RenshiError < StandardError; end
+  
+  class Statement
+    attr_accessor :str
+    attr_accessor :context
+    attr_accessor :interpreted_string
+    
+    def initialize(str, context)
+      @str = str
+      @context = context
+    end
+    
+    def interpret!
+      @interpreted_string = eval self.str, self.context
+    end    
+  end
 
   #helper methods for Renshi on the Nokogiri XMLNode
   module Node
@@ -76,6 +78,45 @@ module Renshi
         commands << [key, self.attributes[key]] if key[0..1] == "r:"
       end
       return commands
+    end
+    
+    def interpret(context) 
+      idx = self.text.index("$")
+      return self.text if idx.nil?
+      
+      bits = []
+      bits << self.text[0..idx] if idx != 0
+      while idx != nil do
+        if self.text[(idx + 1)..(idx + 1)] == "{"
+          begin
+            closing_brace_idx = self.text().index("}")
+            statement_str = self.text[(idx + 2)..(closing_brace_idx -1)]
+            statement = Renshi::Statement.new(statement_str, context)
+            bits << statement.interpret!
+            end_statement_idx = closing_brace_idx + 1
+          rescue StandardError => boom
+            raise RenshiError, "No closing brace: #{self.text()[(idx +1)..-1]}", caller
+          end
+        else #$foo
+          words = self.text()[(idx +1)..-1].split(/\s/)
+          statement_str = words.first
+          statement = Statement.new(statement_str, context)
+          bits << statement.interpret!
+          end_statement_idx = (words.first.length) + 1 + idx
+        end
+
+        next_statement_idx = self.text().index("$", end_statement_idx)
+                
+        if next_statement_idx
+          gap = self.text()[end_statement_idx..(next_statement_idx -1)]
+          bits << gap
+        else
+          bits << self.text()[end_statement_idx..-1]
+        end
+        idx = next_statement_idx
+      end       
+      
+      return bits.join
     end
   end
 end
--- spec/node_spec.rb
+++ spec/node_spec.rb
@@ -3,12 +3,46 @@ require 'nokogiri'
 
 describe Renshi::Node do
 
-  it "should return the commands in an XML element's attributes" do
-    doc = Nokogiri::HTML("<span r:if='true' r:while='true'/>")
-    
+  # it "should return the commands in an XML element's attributes" do
+  #   doc = Nokogiri::HTML("<span r:if='true' r:while='true'/>")
+  #   
+  #   body = doc.root.children.first
+  #   node = body.children.first
+  #   node.attributes.size.should eql 2
+  #   node.commands.size.should eql 2
+  # end
+  # 
+  # it "should interpret $foo" do
+  #   foo = "hello world"
+  #   doc = Nokogiri::HTML("$foo")
+  #   
+  #   body = doc.root.children.first
+  #   node = body.children.first
+  #   node.interpret(binding).should == foo
+  # end
+  #   
+  # it "should interpret $foo $bar" do
+  #   foo = "hello"
+  #   bar = "world"
+  #   doc = Nokogiri::HTML("$foo $bar")
+  #   
+  #   body = doc.root.children.first
+  #   node = body.children.first
+  #   node.interpret(binding).should == "#{foo} #{bar}"
+  # end
+  
+  it "should interpret ${1 + 1}" do
+    doc = Nokogiri::HTML("${1 + 1}")
     body = doc.root.children.first
     node = body.children.first
-    node.attributes.size.should eql 2
-    node.commands.size.should eql 2
+    node.interpret(binding).should eql "2"
+  end  
+  
+  it "should interpret ${1 + 1} $foo" do
+    foo = "is a number"
+    doc = Nokogiri::HTML("${1 + 1} $foo")
+    body = doc.root.children.first
+    node = body.children.first
+    node.interpret(binding).should eql "2 is a number"
   end
 end
