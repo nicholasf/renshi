deaa6e8b2e1cbc32e44faff718068e3b1ccad880
Nicholas Faiz <nicholas.faiz@gmail.com>
2009-06-03 17:45:45 +1000

    cleaning up if.rb

--- lib/renshi/conditional_expressions/if.rb
+++ lib/renshi/conditional_expressions/if.rb
@@ -3,10 +3,7 @@ module Renshi
     class If
       def evaluate(expression, node)
         node.open_clause("if #{expression}")
-        # before("#{Renshi::Parser::STRING_END} if #{expression}; #{Renshi::Parser::STRING_START}")
         node.close_clause("end")
-        
-        # after("#{Renshi::Parser::STRING_END} end; #{Renshi::Parser::STRING_START}")        
       end
     end
   end
