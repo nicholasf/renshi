2c7efb5cb873ba5c99049aa1f46ba0cedbcdf47e
Nicholas Faiz <nicholas.faiz@gmail.com>
2009-07-10 15:24:08 +1000

    fixing test for each

--- spec/unless_spec.rb
+++ spec/unless_spec.rb
@@ -5,7 +5,9 @@ describe Renshi::Parser do
   it "should evaluate r:unless(false)" do
     doc = Nokogiri::HTML("<span id='red' r:unless='false'>hello</span>")
     compiled = Renshi::Parser.parse(doc.root.to_s)
+    puts compiled
     out = eval(compiled, binding)
-   (doc/"span[@id='red']").text.strip.should eql "hello"
+    out = N(out)
+    (out/"span[@id='red']").text.strip.should eql "hello"
   end   
 end
