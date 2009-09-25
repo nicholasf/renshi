76fc05447784d0ead1d7b1151f07f52b78908756
Nicholas Faiz <nicholas.faiz@gmail.com>
2009-09-25 11:22:02 +1000

    trying to diagnose a weird issue on centos - more tests

--- spec/unless_spec.rb
+++ spec/unless_spec.rb
@@ -3,11 +3,25 @@ require 'nokogiri'
 
 describe Renshi::Parser do      
   it "should evaluate r:unless(false)" do
-    doc = Nokogiri::HTML("<span id='red' r:unless='false'>hello</span>")
+    doc = Nokogiri::HTML("<span id='red' r:unless='foo'>hello</span>")
     compiled = Renshi::Parser.parse(doc.root.to_s)
-    puts compiled
+    foo = false
     out = eval(compiled, binding)
     out = N(out)
     (out/"span[@id='red']").text.strip.should eql "hello"
+
+    foo = true
+    out = eval(compiled, binding)
+    out = N(out)
+    (out/"span[@id='red']").text.strip.should eql ""
   end   
+  
+  it "should ... " do
+    foo = true
+    compiled = compile_file("data/unless.ren")
+    puts compiled
+    out = eval compiled, binding
+  
+    out.should_not =~ /hello/
+  end
 end
